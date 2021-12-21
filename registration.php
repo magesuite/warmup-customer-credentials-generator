e3850edabb57bd91554bff67bfd14089aa8d62b4
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2021-12-21 14:10:54 +0100

    Initial commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageSuite_WarmupCustomerCredentialsGenerator',
+    __DIR__
+);
